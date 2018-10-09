d8af4014c8da380cf5a1aec7cc34d636b52bc477
9cco <fredrik.n.krohg@ntnu.no>
2018-10-09 16:56:07 +0200

    Parallelized more of the thermalization algorithm

--- Source/functions_parallel.jl
+++ Source/functions_parallel.jl
@@ -17,7 +17,7 @@ end
 # --------------------------------------------------------------------------------------------------
 # Perform n MCsweeps and return the final state and an array with energies after each sweep
 function nMCSEnergy(ψ::State, sim::Controls, n::Int64, E₀::Float64)
-    E_array = zeros(n)
+	E_array = Array{Float64,1}(n)
 	E_array[1] = E₀ + mcSweepEn!(ψ,sim)
     for i=2:n
         E_array[i] = E_array[i-1] + mcSweepEn!(ψ,sim)
@@ -30,13 +30,13 @@ end
 # Perform n MCsweeps and return the final state and an array with energies after each sweep
 # and updates simulation constants dynamically
 function nMCSEnergyDynamic(ψ::State, sim::Controls, n::Int64, adjust_int::Int64, E₀::Float64)
-    E = zeros(n)
+	Es = Array{Float64,1}(n)
     adjustment_mcs = 0
 
-	E[1] = E₀ + mcSweepEn!(ψ,sim)
+	Es[1] = E₀ + mcSweepEn!(ψ,sim)
     
     for i=2:n
-        E[i] = E[i-1] + mcSweepEn!(ψ,sim)
+        Es[i] = Es[i-1] + mcSweepEn!(ψ,sim)
         
         #Every adjust_int steps, update simulation constants if needed
         if i % adjust_int == 0
@@ -45,10 +45,10 @@ function nMCSEnergyDynamic(ψ::State, sim::Controls, n::Int64, adjust_int::Int64
             adjustment_mcs += mcs
             
             #Update energy (since we do MCS in the update steps)
-            E[i] = E(ψ)
+            Es[i] = E(ψ)
         end
     end
-    return ψ, E, sim, adjustment_mcs
+    return ψ, Es, sim, adjustment_mcs
 end    
 
 #--------------------------------------------------------------------------------------------------
@@ -59,21 +59,50 @@ function adjustSimConstantsPar(sim::Controls, ψ::State, M::Int64=40)
     return ψ, sim, adjustment_mcs
 end
 
+function meanAndStd{T<:Real}(E₁::Array{T,1}, E₂::Array{T,1})
+	dE = E₁ - E₂
+	return mean(dE), std(dE)
+end
+
 #--------------------------------------------------------------------------------------------------
-# Check if average from t_start to t_finish is the same in E_workers and E_ref
-function checkThermalization(E_ref::Array{Float64,1}, E_workers::Array{Float64,2}, Nworkers::Int,
+# Check if average from t_start to t_finish is the same in E_workers and E_ref where these
+# arrays are 
+function checkThermalization(E_ref::Array{Float64,1}, E_workers::Array{Float64,2}, n_workers::Int,
         t_start::Int64, t_end::Int64, STD_NUMBER::Float64=0.1)
     max_av = 0.0
     max_std = 0.0
-    for i=1:Nworkers
-        av = abs(mean(E_ref[t_start:t_end] - E_workers[i,t_start:t_end]))
-        if av > max_av
-            max_av = av
-            max_std = std(E_ref[t_start:t_end] - E_workers[i,t_start:t_end])
-        end
-    end 
-    if max_av <= STD_NUMBER*max_std
-		println("Thermalization successful\nΔE[$(t_start),$(t_end)] = $(av) ± $(std)")
+
+	# Check that we have enough available processes
+	nprocs()-1 >= n_workers || throw(error("ERROR: Not enough workers"))
+
+	future_array = [Future() for i = 1:n_workers]
+	av = Array{Float64,1}(n_workers)
+	st = Array{Float64,1}(n_workers)
+
+	for w = 1:n_workers
+		future_array[w] = @spawn meanAndStd(E_ref[t_start:t_end], E_workers[w,t_start:t_end])
+	end
+	for w = 1:n_workers
+		av[w], st[w] = fetch(future_array[w])
+	end
+
+	# Check if av <= std for all arrays and calculate the worker with the
+	# maximum average
+	thermalized = true
+	for w=1:n_workers
+		if av[w] > st[w]*STD_NUMBER
+			thermlized = false
+		elseif av[w] > max_av
+			max_av = av[w]
+			max_std = st[w]
+		end
+	end
+
+    if thermalized
+		println("Thermalization successful between T = [$(t_start), $(t_end)]")
+		for w = 1:n_workers
+			println("Worker $(w): ΔE = $(abs(av[w])) ± $(st[w])")
+		end
         return true
     else
         return false
@@ -215,7 +244,7 @@ function parallelSFVLA!{T<:Real}(ks::Array{Array{T, 1}, 2},
     nw = M%np
     
     # Make sure that we have enough states
-    length(ψ_list) >= np || throw(Error("ERROR: Not enough states in list"))
+    length(ψ_list) >= np || throw(error("ERROR: Not enough states in list"))
     
     # Setup worker futures
     futures = [Future() for i=1:(np-1)]
@@ -306,24 +335,43 @@ V⁻: \t$(sum(V⁻[rand(1:M)]))")
     return (av_V⁺, err_V⁺, V⁺, av_V⁻, err_V⁻, V⁻, av_S⁺, err_S⁺, S⁺, av_S⁻, err_S⁻, S⁻)
 end
 
+# Find first occurrence of when the values in the arrays cross eachother in the interval
+# between iₛ and iₑ
+function firstZero{T<:Real}(E_high::Array{T, 1}, E_low::Array{T,1}, iₛ::Int64, iₑ::Int64)
+	for i = iₛ:iₑ
+		if E_high[i] <= E_low[i]
+			return i
+		end
+	end
+	return -1
+end
+
 #--------------------------------------------------------------------------------------------------
 #Thermalises several states simultaneously on multiple cores. One high T state is thermalised
 # along with (#available cores - 1) low T states, where energies are compared to ensure proper
 # convergence.
-function parallelThermalisation!(ψ_ref::State, ψ_w::Array{State,1}, c::SystConstants,
-        sim::Controls, T::Int64=1000, ex::Float64=1.5, STD_NUMBER::Float64=0.5)
+function parallelThermalization!(ψ_ref::State, ψ_w::Array{State,1}, c::SystConstants,
+        sim::Controls, T::Int64=1000, ex::Float64=1.8, STD_NUMBER::Float64=0.5)
     CUTOFF_MAX::Int64=4000000           #Max number of MCS before the function terminates
     ADJUST_INTERVAL=400                 #Number of MCS between each sim_const adjustment while finding dE<0
-    AVERAGING_INT=4000                 #Similiar for 2nd loop, also the interval that is averaged over
+    AVERAGING_INT=3000                 #Similiar for 2nd loop, also the interval that is averaged over
 
     #Initialisation
     NWS=length(ψ_w)                     #Number of states in ψ_list
-    E_w = zeros(NWS, CUTOFF_MAX)        #Matrix to store energies of worker states
+	E_w = Array{Float64, 2}(NWS, CUTOFF_MAX)        #Matrix to store energies of worker states
 	for w = 1:NWS
 		E_w[w, 1] = E(ψ_w[w])
 	end
-    E_ref = zeros(CUTOFF_MAX)           #Array to store energies of reference state
+	E_ref = Array{Float64, 1}(CUTOFF_MAX)           #Array to store energies of reference state
 	E_ref[1] = E(ψ_ref)
+
+	# Checking wheather reference or worker has highest energy.
+	ref_highest = [true for i = 1:NWS]
+	for w = 1:NWS
+		if E_w[w,1]>E_ref[1]
+			ref_highest[w] = false
+		end
+	end
 
 	T += 1								# T is as a parameter the initial number of steps taken to find the thermalization and
 										# additionally, in the loops it is the time-index a loop should reach.
@@ -332,7 +380,7 @@ function parallelThermalisation!(ψ_ref::State, ψ_w::Array{State,1}, c::SystCon
     sim_w = [copy(sim) for i=1:NWS]       #Simulation constants for the workers
     adjustment_mcs = 0                  #Number of MCsweeps done during dynamic updates sysconst
     mcs_ref = 0                         # Contains number of MCS done in adjustments of ref state
-    mcs_w = zeros(NWS)                  # Contains number of MCS done in adjustments of worker states.
+    mcs_w = zeros(Int64, NWS)                  # Contains number of MCS done in adjustments of worker states.
     E_check_workers = zeros(Int64, NWS) #Check the requirement dE <= 0 for each process
     tₛ=2                                # First free time-index after last update of energy array.
     thermalized_init = false
@@ -340,7 +388,7 @@ function parallelThermalisation!(ψ_ref::State, ψ_w::Array{State,1}, c::SystCon
 
     #Find the number of available workers
     np = nprocs()-1
-    println("np = $(np)")
+    println("Number of parallel workers: $(np)")
     #Check if we have more low T states than workers
     if NWS > np
         error("Not enough available workers, $(np) workers and $(NWS) states")
@@ -353,10 +401,10 @@ function parallelThermalisation!(ψ_ref::State, ψ_w::Array{State,1}, c::SystCon
     while !thermalized_init && T < CUTOFF_MAX 
         #Run T MCsweeps for all the workers with dynamic sysconst updates
         for w=1:NWS
-            ψ_future_list[w] = @spawn nMCSEnergyDynamic(ψ_w[w], sim_w[w], T-tₛ+1, ADJUST_INTERVAL)
+			ψ_future_list[w] = @spawn nMCSEnergyDynamic(ψ_w[w], sim_w[w], T-tₛ+1, ADJUST_INTERVAL, E_w[w,tₛ-1])
         end
         #Similar for the high T in master
-        ψ_ref, E_ref[tₛ:T], sim_ref, mcs_ref = nMCSEnergyDynamic(ψ_ref, sim_ref, T-tₛ+1, ADJUST_INTERVAL)
+		ψ_ref, E_ref[tₛ:T], sim_ref, mcs_ref = nMCSEnergyDynamic(ψ_ref, sim_ref, T-tₛ+1, ADJUST_INTERVAL, E_ref[tₛ-1])
         
         #Gather results from workers
         for w=1:NWS
@@ -364,15 +412,21 @@ function parallelThermalisation!(ψ_ref::State, ψ_w::Array{State,1}, c::SystCon
         end
         adjustment_mcs += max(maximum(mcs_w),mcs_ref)
         
-        #Check if for all processes dE < 0, and get largest step that this happens for
+        # Check if for all processes dE < 0, and get largest step that this happens for.
+		# Takes into account whether it is the worker or reference that should have highest energy.
 		for w = 1:NWS
-			for i = tₛ+1:T
-				if E_ref[i] - E_w[w,i] <= 0.0
-                    E_check_workers[w] = i
-                    println("Worker $(w) initially thermalised after $(i) steps")
-                    flush(STDOUT)
-					break
-				end
+			if ref_highest[w]
+				ψ_future_list[w] = @spawn firstZero(E_ref, E_w[w, :], tₛ, T)
+			else
+				ψ_future_list[w] = @spawn firstZero(E_w[w,:], E_ref, tₛ, T)
+			end
+		end
+		for w = 1:NWS
+			i = fetch(ψ_future_list[w])
+			if i != -1
+                E_check_workers[w] = i
+                println("Worker $(w) initially thermalised after $(i) steps")
+                flush(STDOUT)
 			end
 		end
 
@@ -380,6 +434,7 @@ function parallelThermalisation!(ψ_ref::State, ψ_w::Array{State,1}, c::SystCon
             tₛ = T
             T = min(Int(ceil(T*ex)), CUTOFF_MAX)
 			println("Increasing simulation time such that tₛ = $(tₛ) and T = $(T)")
+			flush(STDOUT)
             if T == CUTOFF_MAX
                 println("Failed to find a point where ΔE <= 0")
                 return (-1, E_ref[1:tₛ], E_w[:,1:tₛ], ψ_ref, ψ_w, sim_ref, sim_w)
@@ -387,6 +442,7 @@ function parallelThermalisation!(ψ_ref::State, ψ_w::Array{State,1}, c::SystCon
         else
             tₛ = maximum(E_check_workers)
             println("All workers initially thermalized after $(tₛ) steps")
+			flush(STDOUT)
             thermalized_init = true
         end
     end
@@ -407,14 +463,20 @@ function parallelThermalisation!(ψ_ref::State, ψ_w::Array{State,1}, c::SystCon
             ψ_w[w], sim_w[w], mcs_w[w] = fetch(ψ_future_list[w]) 
         end
         adjustment_mcs += max(maximum(mcs_w),mcs_ref)
+
+		# Correct energy for built-up floating point errors
+		E_ref[tₛ-1] = E(ψ_ref)
+		for w=1:NWS
+			E_w[w,tₛ-1] = E(ψ_w[w])
+		end
         
         # Do MCS to find average
         for w=1:NWS
-            ψ_future_list[w] = @spawn nMCSEnergy(ψ_w[w], sim_w[w], AVERAGING_INT)
+			ψ_future_list[w] = @spawn nMCSEnergy(ψ_w[w], sim_w[w], AVERAGING_INT, E_w[w,tₛ-1])
         end
-        ψ_ref, E_ref[tₛ:T] = nMCSEnergy(ψ_ref, sim_ref, AVERAGING_INT)
-        for i=1:NWS
-            ψ_w[i], E_w[i,tₛ:T] = fetch(ψ_future_list[i])
+		ψ_ref, E_ref[tₛ:T] = nMCSEnergy(ψ_ref, sim_ref, AVERAGING_INT, E_ref[tₛ-1])
+        for w=1:NWS
+            ψ_w[w], E_w[w,tₛ:T] = fetch(ψ_future_list[w])
         end
 
         if checkThermalization(E_ref/N, E_w./N, NWS, tₛ, T,STD_NUMBER)
@@ -422,7 +484,7 @@ function parallelThermalisation!(ψ_ref::State, ψ_w::Array{State,1}, c::SystCon
         end
         tₛ=T + 1
         T = T + AVERAGING_INT - 1
-        println("Failed to find proper therm, increasing simulation time to T = $(T)")
+        println("Average too high, increasing simulation time to T = $(T)")
     end
     return -1, E_ref, E_w, ψ_ref, ψ_w, sim_ref, sim_w
 end
