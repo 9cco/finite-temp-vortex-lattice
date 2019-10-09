a4988a64cd22566e1aeaf9a803193103a429e8cd
9cco <fredrik.n.krohg@ntnu.no>
2019-10-09 11:46:00 +0200

    Updated post_temp_series.jl to be able to calculate results for symmetric Landau gauge. This is now again outdated because of the inclusion of kappa in SystConstants

--- Scripts/post_temp_series.jl
+++ Scripts/post_temp_series.jl
@@ -20,16 +20,16 @@ function fixRC()
 end
 fixRC()
 
-src_path = "/home/subr00t/mc/Source/Grid/"
+src_path = "/home/nicolai/mc/Source/Grid/"
 @everywhere push!(LOAD_PATH, $src_path)
 @everywhere using CuboidModule
 
-@everywhere src_path = "/home/subr00t/mc/Source/Grid/"
+@everywhere src_path = "/home/nicolai/mc/Source/Grid/"
 @everywhere include(src_path*"observables.jl")
 @everywhere include(src_path*"utilities.jl")
 include(src_path*"plot_functions.jl")
 
-@everywhere src_path = "/home/subr00t/mc/Source/"
+@everywhere src_path = "/home/nicolai/mc/Source/"
 @everywhere include(src_path*"jackknife_estimates.jl")
 
 using Plots
@@ -39,7 +39,12 @@ using JLD
 
 plot_folder = "Temp_Series"
 
-folders = filter(s -> isdir(s) && s != plot_folder, readdir())
+#folders = filter(s -> isdir(s) && s != plot_folder, readdir())
+Ts = [2.0, 1.8, 1.7, 1.6, 1.5, 1.4, 1.3, 1.2]
+folders = Array{AbstractString, 1}(undef, 0)
+for t in Ts
+    push!(folders, "full_model_L=32_T=$(t)_g=0.3_nu=0.0_n=-1_m=0_mult_g")
+end
 num_blocks = 2^7
 N_F = length(folders)
 
@@ -57,6 +62,8 @@ control_err_T = Array{Float64, 1}(undef, N_F)
 T_list = Array{Float64, 1}(undef, N_F)
 u⁺_by_T = Array{Float64, 1}(undef, N_F)
 u⁻_by_T = Array{Float64, 1}(undef, N_F)
+u²_diff_by_T = Array{Float64, 1}(undef, N_F)
+u²_diff_err_by_T = Array{Float64, 1}(undef, N_F)
 E_avg_by_T = Array{Float64, 1}(undef, N_F)
 E_err_by_T = Array{Float64, 1}(undef, N_F)
 
@@ -64,8 +71,9 @@ E_err_by_T = Array{Float64, 1}(undef, N_F)
 cd(folders[1])
 
 meta_di = JLD.load("meta.jld")
-f = meta_di["f"]; L₃ = meta_di["L3"]; M = meta_di["M"]; κ₅ = meta_di["kappa"];
+m = meta_di["m"]; n = meta_di["n"]; L₃ = meta_di["L3"]; M = meta_di["M"]; κ₅ = meta_di["kappa"];
 L₁ = meta_di["L1"]; L₂ = meta_di["L2"]; g = meta_di["g"]; ν = meta_di["nu"]
+f = n/L₁ - m/L₂
 N = L₁*L₂*L₃;
 
 cd("../")
@@ -76,18 +84,17 @@ for (i_f, folder) = enumerate(folders)
     # Calculating specific heat
 
     ens_di = JLD.load("energies.jld")
-    E_by_T = ens_di["E_by_T"]
+    Es = ens_di["Es"]
 
     meta_di = JLD.load("meta.jld")
-    f_new = meta_di["f"]; L₃_new = meta_di["L3"]; M_new = meta_di["M"]; temps = meta_di["temps"]; κ₅_new = meta_di["kappa"];
+    m_new = meta_di["m"]; n_new = meta_di["n"]; L₃_new = meta_di["L3"]; M_new = meta_di["M"]; T = meta_di["T"]; κ₅_new = meta_di["kappa"];
     Δt = meta_di["dt"]; L₁_new = meta_di["L1"]; L₂_new = meta_di["L2"]; g_new = meta_di["g"]; ν_new = meta_di["nu"]
     M_amp = meta_di["M_amp"];
-    println("Calculating Cv and structure at T = $(temps[1])")
+    println("Calculating Cv and structure at T = $(T)")
 
     N = L₁*L₂*L₃;
-    N_T = length(temps)
 
-    if N_T != 1 || f_new != f || L₁_new != L₁ || L₂_new != L₂ || L₃_new != L₃ || κ₅_new != κ₅ || g_new != g || ν_new != ν
+    if n_new != n || m_new != m || L₁_new != L₁ || L₂_new != L₂ || L₃_new != L₃ || κ₅_new != κ₅ || g_new != g || ν_new != ν
         println("ERROR: encountered simulation of multiple simultaneous temperatures or changed parameters.")
         exit()
     end
@@ -96,37 +103,37 @@ for (i_f, folder) = enumerate(folders)
         println("Warning: one of the measurements has M = $(M_new) vs the previous M = $(M)")
     end
 
-    jv = jackVars(energies -> specificHeat(energies, 1/temps[1]), E_by_T[1], num_blocks; skip_check=true)
+    jv = jackVars(energies -> specificHeat(energies, 1/T), Es, num_blocks; skip_check=true)
     Cv_avg_by_T[i_f], var = jackEstimate(jv)
     Cv_err_by_T[i_f] = √(var)
-    T_list[i_f] = temps[1]
+    T_list[i_f] = T
 
     # Calculating average energy
 
-    jv = jackVars(mean, E_by_T[1]./N, num_blocks; skip_check=true)
+    jv = jackVars(mean, Es./N, num_blocks; skip_check=true)
     E_avg_by_T[i_f], var = jackEstimate(jv)
     E_err_by_T[i_f] = √(var)
 
     # Calculating S⁺
     
     vo_di = JLD.load("vorticity.jld")
-    S⁺_by_T = vo_di["sp"];
+    S⁺_meas = vo_di["sp"];
     # Measured k-vector components3
     kx = [two_pi/L₁*(x-1-L₁/2) for x = 1:L₁]
     ky = [two_pi/L₂*(y-1-L₂/2) for y = 1:L₂];
     # Normalizing structure function
     normalization = (L₁*L₂*f*two_pi)^2#(L^2*f*two_pi)^2
-    S⁺_by_T = [S⁺_measures./normalization for S⁺_measures in S⁺_by_T];
+    S⁺_meas = S⁺_meas./normalization
 
     # Singeling out measurement series for a specific point (n₁, n₂)
     (n₁, n₂) = (Int(L₁/2)-1, 5+Int(L₂/2))
-    hex_series = [fourier_lattice[n₁, n₂] for fourier_lattice in S⁺_by_T[1]]
+    hex_series = [fourier_lattice[n₁, n₂] for fourier_lattice in S⁺_meas]
     (n₁, n₂) = (Int(L₁/2)-3, 4+Int(L₂/2))
-    quad_series = [fourier_lattice[n₁, n₂] for fourier_lattice in S⁺_by_T[1]]
+    quad_series = [fourier_lattice[n₁, n₂] for fourier_lattice in S⁺_meas]
     (n₁, n₂) = (Int(L₁/2)+3, 4+Int(L₂/2))
-    stripe_series = [fourier_lattice[n₁, n₂] for fourier_lattice in S⁺_by_T[1]]
+    stripe_series = [fourier_lattice[n₁, n₂] for fourier_lattice in S⁺_meas]
     (n₁, n₂) = (Int(L₁/2), 1+Int(L₂/2))
-    control_series = [fourier_lattice[n₁, n₂] for fourier_lattice in S⁺_by_T[1]]
+    control_series = [fourier_lattice[n₁, n₂] for fourier_lattice in S⁺_meas]
 
     jv_hex = jackVars(mean, hex_series, num_blocks; skip_check=true)
     jv_quad = jackVars(mean, quad_series, num_blocks; skip_check=true)
@@ -146,10 +153,18 @@ for (i_f, folder) = enumerate(folders)
     # Calculating average amplitudes
 
     amplitude_di = JLD.load("amplitudes.jld");
-    u⁻_avg_lattices = amplitude_di["um_xy"];
-    u⁺_avg_lattices = amplitude_di["up_xy"];
-    u⁺_by_T[i_f] = mean(u⁺_avg_lattices[1]./M_new)
-    u⁻_by_T[i_f] = mean(u⁻_avg_lattices[1]./M_new)
+    u⁻_avg_lattice = amplitude_di["um_xy"];
+    u⁺_avg_lattice = amplitude_di["up_xy"];
+    u⁺_by_T[i_f] = mean(u⁺_avg_lattice./M_new)
+    u⁻_by_T[i_f] = mean(u⁻_avg_lattice./M_new)
+
+    # Calculating average absolute difference
+    u⁺_avg_meas = amplitude_di["up_avg"]
+    u⁻_avg_meas = amplitude_di["um_avg"]
+    u²_diff_meas = [abs(u⁺_avg_meas[m]^2 - u⁻_avg_meas[m]^2) for m = 1:length(u⁺_avg_meas)]
+    jv = jackVars(mean, u²_diff_meas, num_blocks; skip_check=true)
+    u²_diff_by_T[i_f], u²_diff_var = jackEstimate(jv)
+    u²_diff_err_by_T[i_f] = √(u²_diff_var)
 
 
     cd("../")
@@ -179,6 +194,12 @@ plt = scatter(1 ./T_list, abs.(u⁺_by_T.-u⁻_by_T); ylabel=L"\|\langle\bar{u}^
               title="g = $(round(g; digits=2)), ν = $(round(ν; digits=2)), fL₁ = $(round(f*L₁; digits=2)), L₁=$(L₁)",
               label="", xlims=(0, maximum(1 ./T_list)*1.05))
 savefig(plt, "ampdiff_by_beta.pdf")
+
+plt = scatter(T_list, u²_diff_by_T; yerror=u²_diff_err_by_T, ylabel=L"\langle(\|\bar{u}^+)^2 - (\bar{u}^-)^2\|\rangle", xlabel="T",
+              title="g = $(round(g; digits=2)), ν = $(round(ν; digits=2)), fL₁ = $(round(f*L₁; digits=2)), L₁=$(L₁)",
+              label=L"u^+", xlims=(0,maximum(T_list)*1.05))
+scatter!(plt, T_list, u⁻_by_T; label=L"u^-")
+savefig(plt, "amp2diff_by_T.pdf")
 
 # Plotting structure factor
 
